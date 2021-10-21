ce8b38a9cc76492a730a837b150ac960cb80566a
qiokis <70791542+qiokis@users.noreply.github.com>
2021-10-21 18:06:15 +0300

    Now works

--- to_excel.py
+++ to_excel.py
@@ -1,14 +1,34 @@
 import os
+import pandas
 
-def create_excel(output_file_name, sheet_name):
-    directory = "earthquake_data/"
-    first = directory + os.listdir(directory)[0]
-    data = pandas.DataFrame(pandas.read_json(first))
-    for file in os.listdir(directory)[1:]:
-        next = pandas.DataFrame(pandas.read_json(directory+file))
-        data = data.append(next)
-    data.to_excel(output_file_name, sheet_name=sheet_name)
+
+default_directory = "earthquake_data"
+
+
+def create_excel(output_file_name, sheet_name, data_directory=default_directory, file_name=''):
+    """
+    Function creating excel file and place there info from json file or
+     info from whole directory with json files
+    :param output_file_name: Name of excel file
+    :param sheet_name: Name of excel sheet
+    :param data_directory: Name of directory with json files
+    :param file_name: Name of exact json file (optional)
+    :return:
+    """
+    directory = data_directory
+    if file_name:
+        data = pandas.DataFrame(pandas.read_json(f"{directory}\\{file_name}"))
+    else:
+        first = f"{directory}\\{os.listdir(directory)[0]}"
+        data = pandas.DataFrame(pandas.read_json(first))
+        for file in os.listdir(directory)[1:]:
+            next = pandas.DataFrame(pandas.read_json(f"{directory}\\{file}"))
+            data = data.append(next)
+    data.to_excel(output_file_name+'.xlsx', sheet_name=sheet_name)
 
 
 if __name__ == "__main__":
-    create_excel()
+    create_excel(input("Enter name of excel file: "),
+                 input("Enter name of excel sheet: "),
+                 input("Enter name of directory with data: "),
+                 input("Enter name of file with data (optional): "))
