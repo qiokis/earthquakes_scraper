d8764db64725578ca9b2da003fc0cba29a908e58
qiokis <70791542+qiokis@users.noreply.github.com>
2021-10-23 15:46:04 +0300

    Configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+site = "http://www.gsras.ru/ftp/Teleseismic_Catalog/"
+
+logger_file = "log.log"
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+date_format = "%d/%m/%Y %I:%M:%S"
+
+catalog_file = "catalog.json"
