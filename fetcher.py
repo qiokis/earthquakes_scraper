70b5ea9645e459935ab3224cd1c7333b8fea4765
qiokis <70791542+qiokis@users.noreply.github.com>
2021-10-25 14:04:00 +0300

    A few fixes

--- fetcher.py
+++ fetcher.py
@@ -2,21 +2,16 @@ import re
 import requests as req
 from bs4 import BeautifulSoup
 import logging
-import config
+import config as c
 
 from file_writer import FileWriter
 
 site = "http://www.gsras.ru/ftp/Teleseismic_Catalog/"
 max_attempts = 5
 
-logging.basicConfig(format=config.log_format, filename=config.logger_file,
-                    datefmt=config.date_format, filemode="w", level=logging.INFO)
+logging.basicConfig(format=c.log_format, filename=c.logger_file,
+                    datefmt=c.date_format, filemode="w", level=logging.INFO)
 logger = logging.getLogger("Fetcher")
-
-
-YEAR_PATTERN = re.compile(r"\d{4}\.txt")
-YEAR_MONTH_PATTERN = re.compile(r"\d{2}m\d{2}\.txt")
-YEAR_MONTH_SHARDED_PATTERN = re.compile(r"\d{2}d\d{3}sb\.txt")
 
 
 class Fetcher:
@@ -35,6 +30,8 @@ class Fetcher:
         res = req.get(self.main_catalog)
         soup = BeautifulSoup(res.text, "lxml")
 
+        logger.info(f"Fetching from {c.site} started")
+
         for el in soup.find_all("td"):
 
             file_pattern = re.compile(r"\d{4}\.txt")
@@ -45,18 +42,19 @@ class Fetcher:
                 temp = self.main_catalog + el.text
                 res = req.get(temp)
                 soup1 = BeautifulSoup(res.text, "lxml")
+                print(el)
 
                 for el1 in soup1.find_all("td"):
-                    if any([YEAR_PATTERN.match(el1.text),
-                           YEAR_MONTH_PATTERN.match(el1.text),
-                           YEAR_MONTH_SHARDED_PATTERN.match(el1.text)]):
+                    if any([c.YEAR_PATTERN.match(el1.text),
+                           c.YEAR_MONTH_PATTERN.match(el1.text),
+                           c.YEAR_MONTH_SHARDED_PATTERN.match(el1.text)]):
                         self.fetch_link_file(el.text + el1.text)
 
             elif file_pattern.match(el.text):
                 self.fetch_link_file(el.text)
 
         try:
-            self.fw.write(self.files)
+            self.fw.write(self.files, c.catalog_file)
             logger.info("Catalog was successfully fetched")
         except BaseException as error:
             logger.error(error)
@@ -65,21 +63,23 @@ class Fetcher:
         # Site had changed structure of files for 3 times, so here 3 types of proceeding
         # For files, where file have only year in name
         file_name = name.split('/')[-1]
-        if YEAR_PATTERN.match(file_name):
-            self.files.append({'link': self.main_catalog + name,
-                               'type': 0})
+        file_type = -1
+        if c.YEAR_PATTERN.match(file_name):
+            file_type = 0
         # For files, where file have year and month in name
-        elif YEAR_MONTH_PATTERN.match(file_name):
-            self.files.append({'link': self.main_catalog + name,
-                               'type': 1})
+        elif c.YEAR_MONTH_PATTERN.match(file_name):
+            file_type = 1
         # For files, where file have year, month and part of month in name
-        elif YEAR_MONTH_SHARDED_PATTERN.match(file_name):
-            self.files.append({'link': self.main_catalog + name,
-                               'type': 2})
+        elif c.YEAR_MONTH_SHARDED_PATTERN.match(file_name):
+            file_type = 2
+        self.files.append({'link': self.main_catalog + name,
+                           'type': file_type})
 
-    def fetch_file(self, file_link):
+    @staticmethod
+    def fetch_file(file_link):
         flag = False
-        for i in range(config.max_attempts):
+        res = None
+        for i in range(c.max_attempts):
             try:
                 logger.info("Attempt:%d | Trying fetch data from %s" % (i, file_link))
                 res = req.get(file_link, timeout=2)
@@ -91,6 +91,5 @@ class Fetcher:
         if not flag:
             return -1
         logger.info("Fetching successful")
-        res = req.get(file_link)
         soup = BeautifulSoup(res.text, "lxml")
         return soup.text
