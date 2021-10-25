897c325661c966b9ea66b5f8d599d311a6952ff0
qiokis <70791542+qiokis@users.noreply.github.com>
2021-10-25 14:04:14 +0300

    A few fixes

--- file_writer.py
+++ file_writer.py
@@ -1,6 +1,9 @@
 import json
+import os
+
 import config
 import logging
+from bs4 import BeautifulSoup
 
 
 logging.basicConfig(format=config.log_format, filename=config.logger_file,
@@ -10,12 +13,18 @@ logger = logging.getLogger("FileWriter")
 
 class FileWriter:
 
-    file_name = config.catalog_file
+    # file_name = config.catalog_file
 
-    def write(self, data):
+    def write(self, data, file_name, dir_name=''):
+        if dir_name and os.getcwd().split('\\')[-1] != dir_name:
+            try:
+                os.chdir(dir_name)
+            except BaseException as error:
+                os.mkdir(dir_name)
+                os.chdir(dir_name)
         try:
-            with open(self.file_name, 'w') as json_file:
+            with open(file_name, 'w') as json_file:
                 json.dump(data, json_file)
             logger.info("Data was successfully recorded")
         except BaseException as error:
-            logger.error(error)
+            logger.error(f"{file_name}-{error}")
