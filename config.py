99d4ddc323dfc4c4d598312145fceb13e6710383
qiokis <70791542+qiokis@users.noreply.github.com>
2021-10-25 13:59:42 +0300

    A few options

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import re
+
 site = "http://www.gsras.ru/ftp/Teleseismic_Catalog/"
+max_attempts = 5
 
 logger_file = "log.log"
 log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-date_format = "%d/%m/%Y %I:%M:%S"
+date_format = "%d-%m-%Y %I:%M:%S"
 
 catalog_file = "catalog.json"
+json_directory = "earthquake_data"
+
+YEAR_PATTERN = re.compile(r"\d{4}(.txt)?")
+YEAR_MONTH_PATTERN = re.compile(r"\d{2}m\d{2}(.txt)?")
+YEAR_MONTH_SHARDED_PATTERN = re.compile(r"\d{2}d\d{3}sb(.txt)?")
